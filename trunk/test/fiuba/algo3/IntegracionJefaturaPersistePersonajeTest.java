aa6bd1effeb71ab322dc57820c9ce881a4688163
juanmlambre@gmail.com <juanmlambre@gmail.com@327643c7-9a04-1329-eec2-15fd581336e5>
2014-07-01 05:17:42 +0000

    Que jodido fue la perseverancia para la escritura! Metodo nuevo: Jefatura >> sumarCasoAXML(File personajesXML, String nombrePersonaje) Suma un caso al personaje con ese nombre. Si no existe, lo agrega al XML. El resto de los nombres los deja igual

--- trunk/test/fiuba/algo3/IntegracionJefaturaPersistePersonajeTest.java
+++ trunk/test/fiuba/algo3/IntegracionJefaturaPersistePersonajeTest.java
@@ -6,16 +6,24 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.junit.After;
 import org.junit.Assert;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class IntegracionJefaturaPersistePersonajeTest {
 	
 	Ladron ladron;
+	String nombreDeArchivo;
 	
 	public IntegracionJefaturaPersistePersonajeTest() throws ParserConfigurationException, SAXException, IOException{
 		ladron = new Ladron();
@@ -42,11 +50,45 @@ public class IntegracionJefaturaPersistePersonajeTest {
 		ladron.robarUnTesoroRandom(tesoros, docPaises);
 	}
 	
+	@Before
+	public void before(){
+		this.nombreDeArchivo = java.util.UUID.randomUUID().toString() + ".xml";
+	}
+	
+	@After
+	public void after(){
+		File archivo = new File(this.nombreDeArchivo);
+		if(archivo.exists())
+			archivo.delete();
+	}
+	
 	@Test
-	public void jefaturaDevuelveUnaInstanciaDePersonaje(){
+	public void jefaturaDevuelveUnaInstanciaDePersonajeYActualiza() throws ParserConfigurationException, TransformerException{
 		Jefatura jefatura = new Jefatura(this.ladron);
-		Personaje personaje = jefatura.asignarPersonajeConNombre("juanma");
+		File personajesXML = new File(this.nombreDeArchivo);
 		
+		//Preparo personajesXML para que contenga <Personajes/>
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.newDocument();
+		Element elementoPersonajes = doc.createElement("Personajes");
+		doc.appendChild(elementoPersonajes);
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(personajesXML);
+		transformer.transform(source, result);
+	
+		//Verifico la escritura
+		jefatura.sumarCasoAXML(personajesXML, "pepito");
+		
+		Assert.assertNotNull(personajesXML.toString());
+		Assert.assertNotEquals("", personajesXML.toString());
+		
+		
+		//Verifico la lectura
+		Personaje personaje = jefatura.asignarPersonajeConNombre(personajesXML, "pepito");
 		Assert.assertNotNull(personaje);
 	}
+
 }
